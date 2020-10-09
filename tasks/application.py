e271c78534f314d48585f4f261f10b07df507b86
alishahwee <alisha.hwee@outlook.com>
2020-10-09 04:12:00 +0000

    Configure '/add' route to append todos and redirect

--- tasks/application.py
+++ tasks/application.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
+
+todos = []
 
 @app.route("/")
 def tasks():
     return render_template("tasks.html")
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    return render_template("add.html")
+    if request.method == "GET":
+        return render_template("add.html")
+    else:
+        todo = request.form.get("task")
+        todos.append(todo)
+        return redirect("/")
     
