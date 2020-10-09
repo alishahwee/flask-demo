36e17c7f47e73a6cf0386fe8e9d68c36dff452ce
alishahwee <alisha.hwee@outlook.com>
2020-10-09 04:13:19 +0000

    Pass todos list to home template

--- tasks/application.py
+++ tasks/application.py
@@ -6,7 +6,7 @@ todos = []
 
 @app.route("/")
 def tasks():
-    return render_template("tasks.html")
+    return render_template("tasks.html", todos=todos)
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
