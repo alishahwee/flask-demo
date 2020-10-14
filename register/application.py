4497db38fa61cf5c4f20f8ef7759527be443e693
alishahwee <alisha.hwee@outlook.com>
2020-10-14 02:57:51 +0000

    Create and test post request to database

--- register/application.py
+++ register/application.py
@@ -1,5 +1,5 @@
 from cs50 import SQL
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -10,3 +10,18 @@ db = SQL("sqlite:///lecture.db")
 def index():
     rows = db.execute("SELECT * FROM registrants")
     return render_template("index.html", rows=rows)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        name = request.form.get("name")
+        email = request.form.get("email")
+        db.execute(
+            "INSERT INTO registrants (name, email) VALUES (:name, :email)",
+            name=name,
+            email=email,
+        )
+        return redirect("/")
